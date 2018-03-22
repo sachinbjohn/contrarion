e9769e5f25968f499a74dbc0c7b67b7a37bc60f6
Sachin Basil John <sachinbjohn@gmail.com>
2018-03-22 23:33:21 +0100

    Add stage for SendTXN TS

--- src/java/org/apache/cassandra/concurrent/StageManager.java
+++ src/java/org/apache/cassandra/concurrent/StageManager.java
@@ -54,6 +54,7 @@ public class StageManager
         stages.put(Stage.MIGRATION, new JMXEnabledThreadPoolExecutor(Stage.MIGRATION));
         stages.put(Stage.MISC, new JMXEnabledThreadPoolExecutor(Stage.MISC));
         stages.put(Stage.READ_REPAIR, multiThreadedStage(Stage.READ_REPAIR, Runtime.getRuntime().availableProcessors()));
+        stages.put(Stage.SENDTXNTS, multiThreadedConfigurableStage(Stage.SENDTXNTS, Runtime.getRuntime().availableProcessors()));
         stages.put(Stage.VERSION_GOSSIP, new JMXEnabledThreadPoolExecutor(Stage.VERSION_GOSSIP));
     }
 
